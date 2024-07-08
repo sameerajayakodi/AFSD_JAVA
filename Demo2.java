0d4657ecfee84b2d7a495f83e5abd280a4899715
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-08 22:17:29 +0530

    Update Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -1,5 +1,16 @@
+
+
+
+
+
+
+
 class Demo2{
 	public static void main(String[] args){
+		
+		int key = 62;
+		items[] ={1,2,6,7,9,10,11,17,20,33,34,36,48,52,55,58,59,60,62,66,69,70,78,89,91,96,100}
+		binarySearch(items,key);
 		System.out.println("Hello Sameera Jayakodi");
 		}
 	
