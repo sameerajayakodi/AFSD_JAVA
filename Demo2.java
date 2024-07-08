f880442aa199e51d85b8a0c667a81911c24a741b
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-08 22:41:39 +0530

    update

--- Demo2.java
+++ Demo2.java
@@ -7,9 +7,14 @@
 
 class Demo2{
 	public static int binarySearch(int items[],int key){
+	int left = 0;
+	int right = items.length-1;
 	
-	int mid =2 ;
-	return -1;
+	while(left <= right){
+		int mid = (int)(right+left)/2;
+		}
+		System.out.println("");
+	return right;
 	}
 	
 	
@@ -19,7 +24,7 @@ class Demo2{
 		int key = 62;
 		int items[] ={1,2,6,7,9,10,11,17,20,33,34,36,48,52,55,58,59,60,62,66,69,70,78,89,91,96,100};
 		binarySearch(items,key);
-		System.out.println("Hello Sameera Jayakodi");
+		System.out.println(t);
 		}
 	
 	}
