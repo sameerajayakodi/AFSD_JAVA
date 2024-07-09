3547a834e0d5d5b3f394bfc47cb488680216afa8
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-09 16:26:51 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,5 +1,15 @@
 class LinearSearch{
+	
+	static int linearSearch(int key , int items[]){
+		
+		}
+	
+	
 	 public static void main(String[] args){
+		 int []items = {4,85,3,2,69,5,2,4,5,8,62,41,20,3,9,6,96,85,74,85,32,20};
+		 int key = 85;
+		 
+		 linearSearch(key , items);
 	  System.out.println("hello");
 	  
 	  }
