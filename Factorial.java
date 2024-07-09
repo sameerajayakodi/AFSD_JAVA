3ecc0eca56ff0db4c50d957dcb2f84f9ec85dcdc
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-09 16:14:18 +0530

    create factorial

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+
+class Factorial{
+	int factorial(int n){
+		if(n==0)
+		return 1;
+		}else(
+		return (n*factorial(n-1));
+		
+	}
+	
+	
+	public static void main(String [] args){
+		
+		factorial(5);
+		
+		
+	
+	}
+	
+	
+	}
+
