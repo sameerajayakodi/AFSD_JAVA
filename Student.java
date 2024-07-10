d51448becb0017f7ebda7a98543e330da7ba2b9e
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-10 22:24:34 +0530

    Update Student

--- Student.java
+++ Student.java
@@ -2,10 +2,10 @@ class Student{
 	public static void main(String[] args){
 	
 	int x =5;
-	int y = 2;
+	int y = 1;
 	int z = x + y;
 	
-	if(z>5){
+	if(z>6){
 		System.out.println("X + y Greater than 5");
 		}else{
 			System.out.println("X + y Less than 5");
