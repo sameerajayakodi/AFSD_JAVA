d6ed70ca3fe6cee5c64ef2fbb249ebc6944e15aa
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-13 10:09:56 +0530

    Create Demo5.java

---
+++ Demo5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+class Demo5{
+	public static void main(String args[]){
+		Scanner scan =  new Scanner(System.in);
+		System.out.print("Enter Your Marks : ");
+		
+		int mark = scan.nextInt();
+		
+		System.out.println("Your Mark is "+ mark);
+		
+		}
+	
+	
+	}
