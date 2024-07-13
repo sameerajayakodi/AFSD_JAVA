9aa99a37844592482849b2d8b1ee9276226025cf
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-13 09:18:28 +0530

    create demo 4

---
+++ Demo4.java
@@ -0,0 +1,13 @@
+class Demo4{
+	public static void main(String args[]){
+		String studentName = "Sameera Jayakodi";
+		double chemistry = 56.2;
+		double physics = 85.5;
+		double maths = 78.3;
+		
+		double average = (chemistry + physics + maths)/3;
+		
+		System.out.println(studentName + " Your Average Mark is : "+average);
+		}
+	
+	}
