4e5c3901703edeb9a7f614d030696e8f86fc8863
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-13 12:06:55 +0530

    update

--- Demo3.java
+++ Demo3.java
@@ -9,7 +9,7 @@ class Demo3{
 		 
 		 
 		 
-		 System.out.println("Sameera");
+		 /*System.out.println("Sameera");
 		  System.out.print("chandika ");
 		   System.out.println("Sameera");
 		   
@@ -17,11 +17,24 @@ class Demo3{
 		   //age --> variable name
 		   //27 ---> date
 		 
-		int studentAge = 27;
+		/*int studentAge = 27;
 		double rainData = 5.24;
 		char mathsGrade = 'A';
 		String myname = "Sameera";
-		boolean isSmoke = false;
+		boolean isSmoke = false;*/
+		
+		
+		byte b1 = 10;
+		int val1 = b1;
+		
+		int val2 = 20;
+		byte b2 = (byte)val2;
+		
+		
+		int y = 129;
+		byte x = (byte)y;
+		
+		System.out.println(x);
 		}
 	
 	}
