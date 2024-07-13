6a003cc959a2eb5811bebd896f96194e77766cc9
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-13 08:55:07 +0530

    create demo 3 and update

---
+++ Demo3.java
@@ -0,0 +1,27 @@
+
+class Demo3{
+	public static void main(String args[]){
+		//single line comment
+		
+		/*multi line
+		 * ...............
+		 * comment*/
+		 
+		 
+		 
+		 System.out.println("Sameera");
+		  System.out.print("chandika ");
+		   System.out.println("Sameera");
+		   
+		   //int ---> data type
+		   //ae --> variable name
+		   //27 ---> date
+		 
+		int studentAge = 27;
+		double rainData = 5.24;
+		char mathsGrade = 'A';
+		String myname = "Sameera";
+		boolean isSmoke = true;
+		}
+	
+	}
