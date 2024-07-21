0e3b967481c4b20cf5569ad61fda9629aa62ed43
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-21 09:43:12 +0530

    Update Demo7.java

--- Demo7.java
+++ Demo7.java
@@ -13,6 +13,27 @@ class Demo7{
 		
 		double newAmount = (amount + amount*0.03);
 		
-		System.out.prinln("Your Total Amount is : "+newAmount);
+		System.out.println("Your Total Amount is : " + newAmount);
+		
+		//pre maid class / software eg Scanner
+		
+		
+		byte val = -126;
+		
+		int v2 = 20;
+		
+		//short v3 = v2;
+		
+		short v3 = (short)v2;
+		
+		short v4 = (byte)v2;
+		
+		//byte -> short -> char -> int -> long -> float -> double   ---> convertion    <----- casting
+		
+		char val1 = 'a';
+		
+		byte val2 = (byte)val1;
+		System.out.println(val2);
 		
 		}
+}
