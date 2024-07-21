8878fe674d1f51bdd929fa23712eb00407337169
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-21 11:02:17 +0530

    Create Demo8.java

---
+++ Demo8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+class Demo8{
+	
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Your Age : ");
+		int age = sc.nextInt();
+		
+		if(age>18){
+			System.out.println("You are Eligible");
+			}
+		
+		}
+	
+	
+}
