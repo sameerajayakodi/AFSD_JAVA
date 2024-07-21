cc7a6abed071c9174e482345d0b03edac72ebe22
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-21 11:45:02 +0530

    Create Demo10.java

---
+++ Demo10.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+class Demo10{
+	
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);}
+		System.out.print("");
+		
+		
+		
+		
+		
+		
+		
+		
+		}
