0e6a74d9102218671a97d133f6ba303c5421d206
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-21 11:23:03 +0530

    Create Demo9.java

---
+++ Demo9.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+class Demo9{
+	
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		//customerName,mainIncome , businessIncome , otherIncome , totalIncome
+		System.out.print("Enter Customer Name : ");
+		String customerName = sc.next();
+		
+		System.out.print("Enter Your Main Income : ");
+		double mainIncome = sc.nextDouble();
+		
+		System.out.print("Enter Your Business Income : ");
+		double businessIncome = sc.nextDouble();
+		
+		System.out.print("Enter Your Other Income : ");
+		double otherIncome = sc.nextDouble();
+		
+		double totalIncome = mainIncome + businessIncome+ otherIncome;
+		
+		if(totalIncome<50000){
+			
+			System.out.print(customerName+ " you are Aswasuma eligible**. ");
+			}
+		
+		
+		
+		}
+	
+	
+}
