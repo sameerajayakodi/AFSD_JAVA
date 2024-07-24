6d612da86f184377692624f64ba0003c7956f4da
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-24 20:20:42 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+
+
+class Test1{
+	public static void main(String[] args){
+		String employeeName;
+		double monthlySalary ,incrementPecentage ,incrementedSalary;
+		
+		}
+	
+	}
+
 
