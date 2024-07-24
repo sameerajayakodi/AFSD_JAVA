3ddac198aa48a7ad524ce873cc8ba6145ff740c6
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-24 20:29:46 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -3,8 +3,30 @@ import java.util.Scanner;
 
 class Test1{
 	public static void main(String[] args){
-		String employeeName;
-		double monthlySalary ,incrementPecentage ,incrementedSalary;
+		String firstEmployeeName, secondEmployeeName , thirdEmployeeName;
+		double empOneMonthlySalary,emptwoMonthlySalary,empThreeMonthlySalary ,incrementPecentage ,empOneIncrementedSalary,empTwoIncrementedSalary,empThreeIncrementedSalary;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter first Employee Name : ");
+		empOneMonthlySalary = sc.nextInt();
+		
+		System.out.print("Enter first Employee Basic Salary : ");
+		emptwoMonthlySalary = sc.nextInt();
+		
+		System.out.print("Enter second Employee Name : ");
+		empOneMonthlySalary = sc.nextInt();
+		
+		System.out.print("Enter second Employee Basic Salary : ");
+		emptwoMonthlySalary = sc.nextInt();
+		
+		System.out.print("Enter third Employee Name : ");
+		empOneMonthlySalary = sc.nextInt();
+		
+		System.out.print("Enter third Employee Basic Salary : ");
+		emptwoMonthlySalary = sc.nextInt();
+		
+		
+		
 		
 		}
 	
