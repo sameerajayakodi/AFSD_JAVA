bdd2b1f981896f2f8a1f1f2f75ad1651c3475423
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-25 07:42:18 +0530

    Update Test3.java

--- Test3.java
+++ Test3.java
@@ -1,9 +1,29 @@
 
 import java.util.Scanner;
 
-class Test1 {
+class Test3 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Enter a Number : ");
+		double number = sc.nextDouble();
+		
+		if(number!=0){
+			if(number>0){
+				//positive
+				System.out.println(number+" is Positive Number");
+				}else{
+					//negative
+					System.out.println(number+" is Negative Number");
+					}
+			
+			}else{
+				//0
+				System.out.print("Value "+number);
+				
+				}
+		
 		}
     
     
