ac86e8c2853220e3289914d2c75e521bdec59675
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-25 14:37:59 +0530

    Create Test7.java

---
+++ Test7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Test7 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+        System.out.print("Enter your name: ");
+        String name = sc.next();
+        
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        
+        System.out.print("Your gender(male or female) : ");
+        String gender = sc.next();
+        
+        System.out.print("Are You Married (y / n) : ");
+        String relationship = sc.next(); 
+        
+        
+        
+		
+		}
+		
+}
+
+
+  
