90b1b0a5732974e935547713985cb74853a7d6f9
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-25 12:23:11 +0530

    Create Test6.java

---
+++ Test6.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Test5 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Related Number of Your Lagna : ");
+	}
+}	
+	
