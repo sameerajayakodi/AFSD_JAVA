bc12909fe4eb984322ac191e1c72824b86b97816
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-25 08:11:27 +0530

    Create Test4.java

---
+++ Test4.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+class Test4 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Month : ");
+		String month = sc.next();
+		
+		/*System.out.print("Enter year : ");
+		int year = sc.nextInt();
+		boolean specialYear = false;
+		
+		if(year%4==0){
+			specialYear =  true;
+			}*/
+		
+			if(month=="january" || month =="march" || month =="may" || month =="july" || month =="september" || month=="november" ){
+				System.out.println(month + " has 31 Days.");
+				}
+			
+	System.out.print("Enter Month : ");
+		
+		
+		}
+
+}
