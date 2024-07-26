ac52aeb094a575c8c0881c21662ed3c1d8aba47c
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-26 19:27:50 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 
+
+
 class BinarySearch {
+	
+public static void binarySearch(int arr[]){
+	
+	}
+	
+	
+	
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int arr[] = {2,3,4,5,6,7,8,9,10};
 		
-		
+		binarySearch(arr);
 	}
 		
 		
