16879c1f62b91b66ee0a2528c97875e70b72eb31
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-26 19:48:26 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 
 class BinarySearch {
 	
-public static int[] binarySearch(int arr[] , int key ){
+public static void binarySearch(int arr[] , int key ){
+	int first  = 0;
+	int last = arr.length-1;
+	int mid = (first+last)/2;
 	
-	return arr;
+	while(first<=last){
+		if(arr[mid] == key){}
+		System.out.println(key+" is founded by index : "+mid);
+		}else if(key < arr[mid]){
+			last = mid - 1;
+			}else{
+				first = mid +1;
+				}
+	
+	
+	
 	}
 	
 	
@@ -15,7 +28,9 @@ public static int[] binarySearch(int arr[] , int key ){
 		Scanner sc = new Scanner(System.in);
 		int arr[] = {2,3,4,5,6,7,8,9,10};
 		int key = 7;
-		int sortedArr[]= binarySearch(arr , key);
+		
+		
+		binarySearch(arr , key);
 	}
 		
 		
