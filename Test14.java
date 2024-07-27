883fcd1016ba21b326fb7df8d6110cd7f040a352
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-07-27 12:23:46 +0530

    Update Test14.java

--- Test14.java
+++ Test14.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+class Test14 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter Your month : ");
+		String month = sc.next();
+		
+		if(month.equalsIgnoreCase("january")){
+			System.out.println(month + " = Duruthu");
+			    
+			    }else if(month.equalsIgnoreCase("february")){
+				System.out.println(month + " = Nawam");
+				}else if(month.equalsIgnoreCase("march")){
+				System.out.println(month + " = Madin");
+				}else if(month.equalsIgnoreCase("april")){
+				System.out.println(month + " = Bak");
+				}else if(month.equalsIgnoreCase("may")){
+				System.out.println(month + " = Wesak");
+				}else if(month.equalsIgnoreCase("june")){
+				System.out.println(month + " = Poson");
+				}else{
+				System.out.println(month + " is Invalid!");	
+					}
+	}
+}	
