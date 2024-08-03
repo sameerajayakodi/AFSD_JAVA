7ff6a4afa9f9a087fdb96a202ef24f0664fda134
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-03 09:11:13 +0530

    Update Test16.java

--- Test16.java
+++ Test16.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 class Test16 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		for(int i=0;i<5;i++){
+			System.out.println("Hello");
+			
+			//variable scope
+			
+			}
 		
 		}
-		
 }		
