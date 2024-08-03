98132fcab5b48d2950d33a73881e49dd0f4798b1
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-03 13:54:02 +0530

    Update Test17.java

--- Test17.java
+++ Test17.java
@@ -4,18 +4,31 @@ class Test17 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int familyCount = 0;
-		for(int i=0;i<18;i++){
-			System.out.print("Enter Income For Family "+ (i+1) + " : ");
-			double income = sc.nextDouble();
+		String dbUsername = "admin";
+		String dbPassword = "admin";
+		boolean isLogged = false;
+		for(int i =0;i<5;i++){
 			
-			if(income<35000.0){
-				familyCount++;
+		System.out.print("Enter Your username : ");
+		String username = sc.next();
+		
+		System.out.print("Enter Your Password : ");
+		String password = sc.next();
+		
+		if(username.equals(dbUsername) && password.equals(dbPassword)){
+			System.out.print("Login Success!");
+			isLogged = true;
+			break;
+			}else{
+				System.out.println("Login Failed! ");
+				
 				}
-			}
-			System.out.println("Family Count is : "+ familyCount);
+		}
+		    if(!isLogged){
+				System.out.print("Account Locked!");
+				}
+			
 			
 		
-		}
-		
+	}	
 }		
