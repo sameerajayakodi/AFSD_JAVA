dc00ffbef0e1a8c51ce90003f5ce6d75fe7c89ff
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-03 09:30:23 +0530

    Update Test16.java

--- Test16.java
+++ Test16.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 class Test16 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		double totalMarks = 0.0;
+		
 		for(int i=0;i<5;i++){
-			System.out.println("Hello");
+			System.out.print("Enter marks for student "+ (i+1) + " : ");
+			double val = sc.nextDouble();
 			
-			//variable scope
-			
+			totalMarks += val;
 			
 			
 			}
