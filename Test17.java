b4beb5e38f9c16d5111f4ae2012e91c6ed24ca90
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-03 09:56:46 +0530

    Update Test17.java

--- Test17.java
+++ Test17.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
-class Test16 {
+class Test17 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int familyCount = 0;
+		for(int i=0;i<18;i++){
+			System.out.print("Enter Income For Family "+ (i+1) + " : ");
+			double income = sc.nextDouble();
+			
+			if(income<35000.0){
+				familyCount++;
+				}
+			}
+			System.out.println("Family Count is : "+ familyCount);
+			
 		
 		}
 		
