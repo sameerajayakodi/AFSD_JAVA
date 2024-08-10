ce8f51a4894b7857a4bc489816ca0329cfb8dd08
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-10 11:17:04 +0530

    Update Test21.java

--- Test21.java
+++ Test21.java
@@ -1,9 +1,16 @@
 
 import java.util.Scanner;
 
-class Test20 {
+class Test21 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int [] ages = new int [5];
+		for(int i=0; i<5;i++){
+			System.out.print("Enter a Value "+(i+1)+" : ");
+			int val = sc.nextInt();
+			ages[i] = val;
+			}
+		
 		
 	}
 }	
