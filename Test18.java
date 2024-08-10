d0d4c58e9d560feba33ecbb31e3a3403d5a7399f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-10 08:49:07 +0530

    Update Test18.java

--- Test18.java
+++ Test18.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 class Test17 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	
-	
+		
+		boolean val2 = true;
+		while(val2){
+			//task
+			
+			if(){
+				val2 = false;
+				}
+			}
 	}
-	
-	}
+}
