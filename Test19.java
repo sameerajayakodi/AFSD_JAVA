9a6716dfb36c6d45bc7a9fdec58755ad7e4835aa
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-10 10:44:47 +0530

    Update Test19.java

--- Test19.java
+++ Test19.java
@@ -5,5 +5,22 @@ class Test19 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Enter Your Value : ");
+		int inputValue = sc.nextInt();
+		
+		
+		int rev = 0;
+		while(inputValue!=0){
+			
+				
+			 int balance = inputValue%10;
+			 rev = rev*10 + balance; 
+			 inputValue/= 10;
+			}
+			
+			System.out.println("revised value = " +rev);
+		}
+			
+		
 	}
-}	
+	
