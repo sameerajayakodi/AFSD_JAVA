2da6feef820d3ad26ab2a34012a2590645d001a9
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-17 09:42:20 +0530

    Create Test25.java

---
+++ Test25.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+class Test24 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int UniqueIds [] = new int [12];
+		string brands [] = new string [12];
+		string models [] = new string [12];
+		
+		
+		System.out.print("Welcome To thr Laptop Shop");
+		
+		switch(val){
+			case 1:
+			      System.out.println("hello...");
+			      break;
+			      
+			case 2:
+					System.out.println("hii.....");
+					break;
+					
+			case 3:
+					System.out.println("ayubowan");
+					break;
+					
+			default:
+					System.out.println("Invalid value");
+			}
+		
+		for(int i = ){
+			
+		}
+	}
+}	
