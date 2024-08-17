d3503eb4e3885bb5c7a4490695999ac7d8759bc9
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-17 08:50:31 +0530

    Update Test24.java

--- Test24.java
+++ Test24.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+class Test24 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double employeesSalary [] = new double [7];
+		for(int i=0;i<7;i++){
+			System.out.print("Enter employee "+(i+1)+" Salary : ");
+			employeesSalary[i] = sc.nextDouble();
+		}
+		
+		for(int i=0; i<7;i++){
+			employeesSalary[i] += employeesSalary[i]*0.12;
+		}
+		
+		for(int i=0 ; i<7;i++){
+			System.out.println("Employee "+(i+1)+ " new Salary is "+ employeesSalary[i]);
+		}
+		
+		}
+ }
