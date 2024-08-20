5cf030de50b8abbe9a469ee02f3896376b02304f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-20 13:54:28 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -1,9 +1,11 @@
-/**
- * One
- */
+import java,util.Scanner;
 public class One {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+	    double Marksarray
+        for (int i = 0; i < 15; i++) {
+            
+        }
+
     }
 }
