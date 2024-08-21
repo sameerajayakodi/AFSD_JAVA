794da8ea304f88d26ee7c59ad6b0c9c61ec2a5cc
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-21 16:19:51 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -2,46 +2,48 @@ import java.util.Scanner;
 public class One {
 
     public static void main(String[] args) {
-        double[][] temperatures = new double[5][30];
-        Scanner scanner = new Scanner(System.in);
-        
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Enter daily temperatures for city " + (i + 1) + ":");
-            for (int j = 0; j < 30; j++) {
-                System.out.print("Day " + (j + 1) + ": ");
-                temperatures[i][j] = scanner.nextDouble();
+ Scanner scanner = new Scanner(System.in);
+
+        int numStudents = scanner.nextInt();
+        int numAssignments = scanner.nextInt();
+
+        int[][] submissionData = new int[numStudents][numAssignments];
+
+        for (int i = 0; i < numStudents; i++) {
+            System.out.println("Student " + (i + 1));
+            for (int j = 0; j < numAssignments; j++) {
+                System.out.print("Assignment " + (j + 1) + " (1 for submitted, 0 for not submitted): ");
+                submissionData[i][j] = scanner.nextInt();
             }
-        }
-        
-        int aboveTemp = 0;
-        double totalTemperature = 0;
-        double totalDays = 5* 30;
-        
-        for (int i = 0; i < 5; i++) {
-            double cityTotalTemperature = 0;
-            
-            for (int j = 0; j <30; j++) {
-                cityTotalTemperature += temperatures[i][j];
-            }
-            
-            double cityAverageTemperature = cityTotalTemperature / 30;
-            totalTemperature += cityTotalTemperature;
-            System.out.println("City " + (i + 1) + ":");
-            System.out.println("  Average Temperature: " + cityAverageTemperature);
-            
-            if (cityAverageTemperature > 30) {
-                aboveTemp++;
-            }
-        }
-        
-        double overallAverageTemperature = totalTemperature / totalDays;
-        System.out.println("Overall Average Temperature: " + overallAverageTemperature);
-        
-        if (aboveTemp >= 3) {
-            System.out.println("Heat Wave Alert!");
-        } else {
-            System.out.println("Normal Weather.");
         }
 
+        int[] totalCompleted = new int[numStudents];
+        for (int i = 0; i < numStudents; i++) {
+            for (int j = 0; j < numAssignments; j++) {
+                totalCompleted[i] += submissionData[i][j];
+            }
+        }
+
+        double totalCompletedAssignments = 0;
+        for (int i = 0; i < numStudents; i++) {
+            totalCompletedAssignments += totalCompleted[i];
+        }
+        double averageCompletionRate = (totalCompletedAssignments / (numStudents * numAssignments)) * 100;
+
+        int numStudentsWith4OrMoreCompleted = 0;
+        for (int i = 0; i < numStudents; i++) {
+            if (totalCompleted[i] >= 4) {
+                numStudentsWith4OrMoreCompleted++;
+            }
+        }
+        double engagementPercentage = (numStudentsWith4OrMoreCompleted / numStudents) * 100;
+        String engagementLevel = (engagementPercentage >= 80) ? "Good Engagement" : "Low Engagement";
+
+        System.out.println("\nStudent ID\tTotal Completed");
+        for (int i = 0; i < numStudents; i++) {
+            System.out.println((i + 1) + "\t\t" + totalCompleted[i]);
+        }
+        System.out.println("\nAverage Completion Rate: " + averageCompletionRate + "%");
+        System.out.println("Engagement Level: " + engagementLevel);
     }
 }
