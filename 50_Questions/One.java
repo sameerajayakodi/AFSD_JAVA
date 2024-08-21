d705bdc426dda5348298ed76b2b4ff41e3ddb217
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-21 15:54:05 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -2,53 +2,51 @@ import java.util.Scanner;
 public class One {
 
     public static void main(String[] args) {
-	   Scanner sc = new Scanner(System.in);
-
-       
-        int[][] steps = new int[25][30];
         
+        int numDays = 30;
+        double temperatureThreshold = 30.0;
+        int citiesThreshold = 3;
         
-  
-        for (int i = 0; i < 25; i++) {
-            System.out.println("Enter steps for participant " + (i + 1) + ":");
+        double[][] temperatures = new double[5][30];
+        Scanner scanner = new Scanner(System.in);
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter daily temperatures for city " + (i + 1) + ":");
             for (int j = 0; j < 30; j++) {
                 System.out.print("Day " + (j + 1) + ": ");
-                steps[i][j] = sc.nextInt();
+                temperatures[i][j] = scanner.nextDouble();
             }
         }
         
-        int participantsMetGoal = 0;
+        int citiesAboveThreshold = 0;
+        double totalTemperature = 0;
+        double totalDays = 5* 30;
         
-     
-        for (int i = 0; i <25; i++) {
-            int totalSteps = 0;
-            int daysMetGoal = 0;
+        for (int i = 0; i < 5; i++) {
+            double cityTotalTemperature = 0;
             
-            for (int j = 0; j < 30; j++) {
-                totalSteps += steps[i][j];
-                if (steps[i][j] >= 1000) {
-                    daysMetGoal++;
-                }
+            for (int j = 0; j <30; j++) {
+                cityTotalTemperature += temperatures[i][j];
             }
             
-            double averageSteps = (double) totalSteps / 30;
-            System.out.println("Participant " + (i + 1) + ":");
-            System.out.println("  Total Steps: " + totalSteps);
-            System.out.println("  Average Steps per Day: " + averageSteps);
+            double cityAverageTemperature = cityTotalTemperature / 30;
+            totalTemperature += cityTotalTemperature;
+            System.out.println("City " + (i + 1) + ":");
+            System.out.println("  Average Temperature: " + cityAverageTemperature);
             
-            if (daysMetGoal >= 20) {
-                participantsMetGoal++;
+            if (cityAverageTemperature > temperatureThreshold) {
+                citiesAboveThreshold++;
             }
         }
         
-       
-        double percentageMetGoal = (double) participantsMetGoal / 25 * 100;
-        if (percentageMetGoal > 70) {
-            System.out.println("Great Effort!");
+        double overallAverageTemperature = totalTemperature / totalDays;
+        System.out.println("Overall Average Temperature: " + overallAverageTemperature);
+        
+        if (citiesAboveThreshold >= citiesThreshold) {
+            System.out.println("Heat Wave Alert!");
         } else {
-            System.out.println("Keep Pushing.");
+            System.out.println("Normal Weather.");
         }
-        
 
     }
 }
