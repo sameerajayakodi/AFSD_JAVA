330648a21b1314c047584ea24f93021944090a6a
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-21 14:45:06 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -3,36 +3,41 @@ public class One {
 
     public static void main(String[] args) {
 		Scanner sc  = new Scanner(System.in);
-	
-       int students = 20;
-        int marks[] = new int[students];
-        int gradeACount	 = 0, gradeBCount = 0, gradeCCount = 0, gradeDCount = 0, gradeFCount = 0;
+	int students = 20;
+        String[] names = new String[students];
+        int[] ages = new int[students];
 
-      
+    
+        int ageGroup1 = 0;
+        int ageGroup2 = 0;
+        int ageGroup3 = 0;
+        int ageGroup4 = 0;
+
         for (int i = 0; i < students; i++) {
-            System.out.print("Enter marks for Student " + (i + 1) + ": ");
-            marks[i] = sc.nextInt();
+            System.out.print("Enter the name of Student " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+            System.out.print("Enter the age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            sc.nextLine();  
 
-
-            if (marks[i] >= 90) {
-                gradeACount++;
-            } else if (marks[i] >= 80) {
-                gradeBCount++;
-            } else if (marks[i] >= 70) {
-                gradeCCount++;
-            } else if (marks[i] >= 60) {
-                gradeDCount++;
-            } else {
-                gradeFCount++;
+         
+            if (ages[i] >= 18 && ages[i] <= 22) {
+                ageGroup1++;
+            } else if (ages[i] >= 23 && ages[i] <= 30) {
+                ageGroup2++;
+            } else if (ages[i] >= 31 && ages[i] <= 40) {
+                ageGroup3++;
+            } else if (ages[i] > 40) {
+                ageGroup4++;
             }
         }
 
        
-        System.out.println("Grade A: " + gradeACount);
-        System.out.println("Grade B: " + gradeBCount);
-        System.out.println("Grade C: " + gradeCCount);
-        System.out.println("Grade D: " + gradeDCount);
-        System.out.println("Grade F: " + gradeFCount);
+ 
+        System.out.println("18-22 years: " + ageGroup1 + " students");
+        System.out.println("23-30 years: " + ageGroup2 + " students");
+        System.out.println("31-40 years: " + ageGroup3 + " students");
+        System.out.println("Above 40 years: " + ageGroup4 + " students");
 
 
     }
