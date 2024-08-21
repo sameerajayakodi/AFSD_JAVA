a3b6d49b28e14eb28d8c237a5cd9f49b9480b20e
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-21 15:48:14 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -2,30 +2,53 @@ import java.util.Scanner;
 public class One {
 
     public static void main(String[] args) {
-		Scanner sc  = new Scanner(System.in);
-	 
-        int highDemandCount = 0;
+	   Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 50; i++) {
-            int totalUnitsSold = 0;
-
+       
+        int[][] steps = new int[25][30];
+        
+        
+  
+        for (int i = 0; i < 25; i++) {
+            System.out.println("Enter steps for participant " + (i + 1) + ":");
             for (int j = 0; j < 30; j++) {
-                totalUnitsSold += sc.nextInt();
-            }
-
-            double averageDailySales = (double) totalUnitsSold / 30;
-            if (averageDailySales > 10) {
-                highDemandCount++;
+                System.out.print("Day " + (j + 1) + ": ");
+                steps[i][j] = sc.nextInt();
             }
         }
-
-        double percentageHighDemand = (double) highDemandCount / 50 * 100;
-        if (percentageHighDemand > 60) {
-            System.out.println("High Demand Products");
+        
+        int participantsMetGoal = 0;
+        
+     
+        for (int i = 0; i <25; i++) {
+            int totalSteps = 0;
+            int daysMetGoal = 0;
+            
+            for (int j = 0; j < 30; j++) {
+                totalSteps += steps[i][j];
+                if (steps[i][j] >= 1000) {
+                    daysMetGoal++;
+                }
+            }
+            
+            double averageSteps = (double) totalSteps / 30;
+            System.out.println("Participant " + (i + 1) + ":");
+            System.out.println("  Total Steps: " + totalSteps);
+            System.out.println("  Average Steps per Day: " + averageSteps);
+            
+            if (daysMetGoal >= 20) {
+                participantsMetGoal++;
+            }
+        }
+        
+       
+        double percentageMetGoal = (double) participantsMetGoal / 25 * 100;
+        if (percentageMetGoal > 70) {
+            System.out.println("Great Effort!");
         } else {
-            System.out.println("Low Demand Products");
+            System.out.println("Keep Pushing.");
         }
-
+        
 
     }
 }
