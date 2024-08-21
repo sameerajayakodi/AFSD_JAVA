3e7792b485afadba644dd31e120cf109e514781b
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-21 13:56:53 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -3,37 +3,35 @@ public class One {
 
     public static void main(String[] args) {
 		Scanner sc  = new Scanner(System.in);
-	    double marksArray[][] = new double[15][5];
-	    double totalMarksArray [] = new double [15];
-        for (int i = 0; i < 15; i++) {
-			System.out.println("--Enter Student "+(i+1)+" Marks--");
-			System.out.println("");
-            for(int j=0;j<5;j++){
-				System.out.print("Enter Subject "+(j+1)+" Marks : ");
-				marksArray[i][j] = sc.nextDouble();
-			}
-        }
+	  double salesArray[] = new double[10];
+        double totalSales = 0;
+
         
-        double totalMarks = 0;
-        for(int i=0;i<15;i++){
-			for(int j=0;j<5;j++){
-				totalMarks += marksArray[i][j];
-			}
-			totalMarksArray[i]=totalMarks;
-		}
-		
-		double averageMarksArray[] = new double[15];
-		for(int i=0;i<15;i++){
-			averageMarksArray[i] = totalMarksArray[i]/5;
-		}
-		
-		for(int i=0;i<15;i++){
-			if(averageMarksArray[i] >= 40.0){
-				System.out.println("Student "+(i+1)+" You are Passed!");
-			}else{
-				System.out.println("Student "+(i+1)+" You are Failed!");
-			}
-		}
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enterfor salesperson " + (i + 1) + ": ");
+            salesArray[i] = sc.nextDouble();
+            totalSales += salesArray[i];
+        }
+
+        
+        double averageSales = totalSales / 10;
+        System.out.println("Average sales: " + averageSales);
+
+        
+        int belowAverageCount = 0;
+        for (int i = 0; i < 10; i++) {
+            if (salesArray[i] < averageSales) {
+                belowAverageCount++;
+            }
+        }
+
+        
+        System.out.println("Number of salespersons with below average sales: " + belowAverageCount);
+
+       
+        if (belowAverageCount > 5) {
+            System.out.println("Sales team needs improvement.");
+        }
 
     }
 }
