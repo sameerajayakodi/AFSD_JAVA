83f2b77c54c8b4eedafed15fba4bd119f345d6d2
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-21 15:04:05 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -3,41 +3,28 @@ public class One {
 
     public static void main(String[] args) {
 		Scanner sc  = new Scanner(System.in);
-	int students = 20;
-        String[] names = new String[students];
-        int[] ages = new int[students];
+	 
+        int highDemandCount = 0;
 
-    
-        int ageGroup1 = 0;
-        int ageGroup2 = 0;
-        int ageGroup3 = 0;
-        int ageGroup4 = 0;
+        for (int i = 0; i < 50; i++) {
+            int totalUnitsSold = 0;
 
-        for (int i = 0; i < students; i++) {
-            System.out.print("Enter the name of Student " + (i + 1) + ": ");
-            names[i] = sc.nextLine();
-            System.out.print("Enter the age of " + names[i] + ": ");
-            ages[i] = sc.nextInt();
-            sc.nextLine();  
+            for (int j = 0; j < 30; j++) {
+                totalUnitsSold += sc.nextInt();
+            }
 
-         
-            if (ages[i] >= 18 && ages[i] <= 22) {
-                ageGroup1++;
-            } else if (ages[i] >= 23 && ages[i] <= 30) {
-                ageGroup2++;
-            } else if (ages[i] >= 31 && ages[i] <= 40) {
-                ageGroup3++;
-            } else if (ages[i] > 40) {
-                ageGroup4++;
+            double averageDailySales = (double) totalUnitsSold / 30;
+            if (averageDailySales > 10) {
+                highDemandCount++;
             }
         }
 
-       
- 
-        System.out.println("18-22 years: " + ageGroup1 + " students");
-        System.out.println("23-30 years: " + ageGroup2 + " students");
-        System.out.println("31-40 years: " + ageGroup3 + " students");
-        System.out.println("Above 40 years: " + ageGroup4 + " students");
+        double percentageHighDemand = (double) highDemandCount / 50 * 100;
+        if (percentageHighDemand > 60) {
+            System.out.println("High Demand Products");
+        } else {
+            System.out.println("Low Demand Products");
+        }
 
 
     }
