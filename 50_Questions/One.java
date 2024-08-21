ad66efb2f6a47b46c8a3eee79a68dfeb809846cc
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-21 13:31:35 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -2,10 +2,26 @@ import java.util.Scanner;
 public class One {
 
     public static void main(String[] args) {
-	    double MarksArray[][] = new double[15][5];
+		Scanner sc  = new Scanner(System.in);
+	    double marksArray[][] = new double[15][5];
+	    double totalMarksArray [] = new double [15];
         for (int i = 0; i < 15; i++) {
-            
+			System.out.println("--Enter Student "+(i+1)+" Marks--");
+			System.out.println("");
+            for(int j=0;j<5;j++){
+				System.out.print("Enter Subject"+(j+1)+" Marks : ");
+				marksArray[i][j] = sc.nextDouble();
+			}
         }
+        
+        
+        double totalMarks = 0;
+        for(int i=0;i<15;i++){
+			for(int j=0;j<5;j++){
+				double totalMarks += marksArray[i][j];
+			}
+			totalMarksArray[i]=totalMarks;
+		}
 
     }
 }
