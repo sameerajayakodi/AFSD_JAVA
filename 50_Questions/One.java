a5c515532fd3f0e926f2c71bad4907eaba589b86
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-21 11:58:49 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -1,8 +1,8 @@
-import java,util.Scanner;
+import java.util.Scanner;
 public class One {
 
     public static void main(String[] args) {
-	    double Marksarray
+	    double MarksArray[][] = new double[15][5];
         for (int i = 0; i < 15; i++) {
             
         }
