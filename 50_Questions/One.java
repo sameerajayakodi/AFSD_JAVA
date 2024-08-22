92e06e766aacaf8ded336a144ddcf11d9dc44339
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-22 22:53:37 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -4,32 +4,25 @@ public class One {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] ratings = new int[150];
-        int[] ratingCounts = new int[5];
-        double totalRating = 0;
+        int[] dailyOccupancy = new int[30];
+        int totalOccupiedBedDays = 0;
 
-        for (int i = 0; i < 150; i++) {
-            System.out.print("Customer " + (i + 1) + " rating (1-5): ");
-            ratings[i] = scanner.nextInt();
-            totalRating += ratings[i];
-            if (ratings[i] >= 1 && ratings[i] <= 5) {
-                ratingCounts[ratings[i] - 1]++;
-            }
+        for (int i = 0; i < 30; i++) {
+            System.out.print("Day " + (i + 1) + ": ");
+            dailyOccupancy[i] = scanner.nextInt();
+            totalOccupiedBedDays += dailyOccupancy[i];
         }
 
-        double averageRating = totalRating / 150.0;
-        System.out.println("Rating Counts:");
-        for (int i = 0; i < 5; i++) {
-            System.out.println((i + 1) + " stars: " + ratingCounts[i]);
-        }
-        System.out.printf("Average Rating:"+averageRating);
+        double averageOccupancyRate = (totalOccupiedBedDays / 30.0) / 100.0 * 100;
+        System.out.printf("Total Occupied Bed-Days:"+totalOccupiedBedDays);
+        System.out.printf("Average Daily Occupancy Rate:"+ averageOccupancyRate);
 
-        if (averageRating >= 4) {
-            System.out.println("Highly Rated Product");
+        if (averageOccupancyRate >= 80) {
+            System.out.println("High Occupancy Rate");
         } else {
-            System.out.println("Product Needs Improvement");
+            System.out.println("Low Occupancy Rate");
         }
 
-        
+       
     }
 }
