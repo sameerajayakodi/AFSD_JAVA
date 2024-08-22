0af9b90c0275e0eb95d4d0b07d10105c81c23494
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-22 23:10:34 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -4,34 +4,28 @@ public class One {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double[][] waterUsage = new double[15][30];
-        double[] totalUsage = new double[15];
-        int apartmentsAbove = 0;
+        int[][] exerciseTime = new int[20][30];
+        int participantsMeetingGoal = 0;
 
-        for (int i = 0; i < 15; i++) {
-            double total = 0;
-            System.out.println("Enter daily water usage (in liters) for Apartment " + (i + 1) + ":");
+        for (int i = 0; i < 20; i++) {
+            int totalMinutes = 0;
+            System.out.println("Enter daily exercise time (in minutes) for Participant " + (i + 1));
             for (int j = 0; j < 30; j++) {
-                waterUsage[i][j] = scanner.nextDouble();
-                total += waterUsage[i][j];
+                exerciseTime[i][j] = scanner.nextInt();
+                totalMinutes += exerciseTime[i][j];
             }
-            totalUsage[i] = total;
-            if (total > 10000) {
-                apartmentsAbove++;
+            double averageMinutes = totalMinutes / 30.0;
+            if (averageMinutes >= 30) {
+                participantsMeetingGoal++;
             }
         }
 
-        System.out.println("Total Water Usage for Each Apartment:");
-        for (int i = 0; i < 15; i++) {
-            System.out.println("Apartment " + (i + 1) + ": Total Usage = " + totalUsage[i] + " liters");
-        }
-
-        if ((apartmentsAbove / 15.0) >= 0.50) {
-            System.out.println("High Water Usage");
+        if ((participantsMeetingGoal / 20.0) > 0.70) {
+            System.out.println("Successful Fitness Challenge");
         } else {
-            System.out.println("Water Usage Within Limits");
+            System.out.println("Challenge Not Fully Achieved");
         }
 
-      
+        
     }
 }
