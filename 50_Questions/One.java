a910fd5f470dc41bb396bbe613552e3cdda5f3da
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-22 23:11:51 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class One {
+public class One{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,7 +9,7 @@ public class One {
 
         for (int i = 0; i < 20; i++) {
             int totalMinutes = 0;
-            System.out.println("Enter daily exercise time (in minutes) for Participant " + (i + 1));
+            System.out.println("Enter daily exercise time (in minutes) for Participant " + (i + 1) + ":");
             for (int j = 0; j < 30; j++) {
                 exerciseTime[i][j] = scanner.nextInt();
                 totalMinutes += exerciseTime[i][j];
