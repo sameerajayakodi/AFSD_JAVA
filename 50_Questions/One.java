d280a60b839e4d8895d6da5425d37e8a2beec6a0
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-22 22:37:09 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -4,44 +4,49 @@ public class One {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        String[] studentNames = new String[20];
-        int[] studentAges = new int[20];
-        
-        
-        int group18To22 = 0;
-        int group23To30 = 0;
-        int group31To40 = 0;
-        int groupAbove40 = 0;
+        int[][] attendance = new int[40][26];
+        int[] daysPresent = new int[40];
+        int perfectAttendanceCount = 0;
+        int employeesAbove20Days = 0;
 
         
-        for (int i = 0; i < 20; i++) {
-            System.out.println("Enter the name of student " + (i + 1) + ": ");
-            studentNames[i] = scanner.nextLine();
-
-            System.out.println("Enter the age of " + studentNames[i] + ": ");
-            studentAges[i] = scanner.nextInt();
-            scanner.nextLine(); 
-
-            
-            if (studentAges[i] >= 18 && studentAges[i] <= 22) {
-                group18To22++;
-            } else if (studentAges[i] >= 23 && studentAges[i] <= 30) {
-                group23To30++;
-            } else if (studentAges[i] >= 31 && studentAges[i] <= 40) {
-                group31To40++;
-            } else if (studentAges[i] > 40) {
-                groupAbove40++;
+        System.out.println("Enter attendance for 40 employees over 26 days (1 for present, 0 for absent):");
+        for (int i = 0; i < 40; i++) {
+            System.out.println("Enter attendance for Employee " + (i + 1) + ": ");
+            for (int j = 0; j < 26; j++) {
+                attendance[i][j] = scanner.nextInt();
+                if (attendance[i][j] == 1) {
+                    daysPresent[i]++;
+                }
             }
         }
 
         
-        System.out.println("Count of Students in Age Groups:");
-        System.out.println("18-22 years: " + group18To22);
-        System.out.println("23-30 years: " + group23To30);
-        System.out.println("31-40 years: " + group31To40);
-        System.out.println("Above 40 years: " + groupAbove40);
+        for (int i = 0; i < 40; i++) {
+            if (daysPresent[i] == 26) {
+                perfectAttendanceCount++;
+            }
+            if (daysPresent[i] > 20) {
+                employeesAbove20Days++;
+            }
+        }
 
         
+        System.out.println("Total Days Present for each Employee:");
+        for (int i = 0; i < 40; i++) {
+            System.out.println("Employee " + (i + 1) + ": " + daysPresent[i] + " days");
+        }
+
+        
+        System.out.println("Number of employees with perfect attendance (26 days): " + perfectAttendanceCount);
+
+        
+        if ((employeesAbove20Days / 40.0) >= 0.8) {
+            System.out.println("Satisfactory Attendance");
+        } else {
+            System.out.println("Attendance Needs Improvement");
+        }
+
+        scanner.close();
     }
 }
