cf629ef21eb03cf67bb63bb3e0f1f6e1b04a987f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-24 10:05:35 +0530

    Create Test29.java

---
+++ Test29.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Test28{
+	public static void main(String [] args){
+		Scanner scanner = new Scanner(System.in);
+	
+		
+	}
+	
+	
+	public static void saveData(){
+		
+	}
+}
