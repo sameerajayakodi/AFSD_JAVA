2fde71033078ccab38faf0d4f5f332c4005b051d
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-24 11:02:08 +0530

    Update One.java

--- 50_Questions/One.java
+++ 50_Questions/One.java
@@ -4,7 +4,7 @@ public class One {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-       import java.util.Scanner;
+       
         
         int [] physicsFirstExam = new int[35];
         int [] physicsSecondExam = new int[35];
