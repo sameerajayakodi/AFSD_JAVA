f692b2cd130e7f1aa0798e93bd40e70144026570
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-24 10:21:47 +0530

    Update Test29.java

--- Test29.java
+++ Test29.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
+import java.util.Calendar;
+ 
+ 
 
-class Test28{
-	public static void main(String [] args){
-		Scanner scanner = new Scanner(System.in);
-	
-		
-	}
-	
-	
-	public static void saveData(){
-		
-	}
+class Test29 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        greeting();
+    }
+
+    public static void greeting() {
+        Calendar c = Calendar.getInstance(); 
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        
+        if(hour < 12) {
+            System.out.println("Good Morning");
+        } else if (hour >= 12 && hour < 15) {
+            System.out.println("Good Afternoon");
+        } else {
+            System.out.println("Good Night");
+        }
+    }
 }
