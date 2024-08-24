6bcae99e50eead204f4dfb253c987f89ffb26d77
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-24 12:02:28 +0530

    Create Test31.java

---
+++ Test31.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+class Test30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[8];
+        for (int i = 0; i < 8; i++) {
+            System.out.print("Enter Number " + (i + 1) + " : ");
+            array[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter Number what you want to search: ");
+        int key = sc.nextInt();
+
+        searchElement(array, key);
+    }
+
+    public static void searchElement(int[] array, int key) {
+        boolean isAvailable = false;
+        for (int i :array) {
+            if (i == key) {
+                isAvailable = true;
+                break;
+            }
+        }
+        System.out.println(isAvailable);
+    }
+}
