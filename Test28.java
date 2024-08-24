a4e602e646e0713f3fc0d74c11bff4e0355d54ea
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-24 09:58:35 +0530

    Create Test28.java

---
+++ Test28.java
@@ -0,0 +1,14 @@
+
+class Test28{
+	public static void main(String [] args){
+		saveData();
+		saveData();
+	}
+	// ()--> parameter bracket
+	// {} --> method scope
+	public static void saveData(){
+		int data1 = 13;
+		int data2 = 12;
+		System.out.println(data1+data2);
+	}
+}
