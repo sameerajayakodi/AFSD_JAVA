3d2d865b44a48993f5039df207937022cb417015
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-01 15:23:20 +0530

    Create Test33.java

---
+++ Test33.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Test33 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        public static void getSum(String x){
+			
+		}
+		
+		public static void getSum(int x){
+			
+		}
+		
+		public static void getSum(int x,int y){
+			
+		}
+        
+     }
+  }
