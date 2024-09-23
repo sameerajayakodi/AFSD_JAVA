d48df522ef9efc86a5a71e290ceb9131f7580a8c
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-23 22:41:57 +0530

    Update Test37.java

--- Test37.java
+++ Test37.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 class Test37{
 	public static void main(String args[]){
-		 for (int i = 0; i <= 'd'; i++) { 
+		 for (int i = null; i <= 'd'; i++) { 
             System.out.println(i);
         }
 		
