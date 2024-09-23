518eed0d1dd49259dcd32d673097cb5a138bbabd
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-23 22:45:25 +0530

    Update Test38.java

--- Test38.java
+++ Test38.java
@@ -1,1 +1,10 @@
 
+class Test38{
+	public static void main(String args[]){
+		String array []= new String['d'];
+		 for (int i = array.length-array.length; i <= 'd'; i++) { 
+            System.out.println(i);
+        }
+		
+	}
+}
