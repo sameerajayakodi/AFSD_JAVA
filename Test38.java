a1009a6b52ac7799e18317f542309ece1b44d8c4
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-23 22:58:14 +0530

    Update Test38.java

--- Test38.java
+++ Test38.java
@@ -5,7 +5,16 @@ class Test38{
 		String studentsArray[][] = new String[10][2];
 		double subjectMarksArray[][] = new double [10][3];
 		
-		System.out.println("Enter Student (i+1) Id : ");
+		for(int i=0;i<10;i++){
+			for(int j=0;i<2;j++){
+				System.out.println("Enter Student (i+1) Id : ");
+				studentsArray[i][j] = sc.next();
+				System.out.println("Enter Student (i+1) Name : ");
+				studentsArray[i][j+1] = sc.next();
+			}
+			
+		}
+		
 		
 	}
 }
