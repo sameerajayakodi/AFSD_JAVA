32a64a7935c8ab9a2d65f18e03f16f8cff4e73c6
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-23 22:40:12 +0530

    Update Test37.java

--- Test37.java
+++ Test37.java
@@ -1,12 +1,9 @@
 import java.util.Random;
 class Test37{
 	public static void main(String args[]){
-		Random rand = new Random();
-		int random;
-		for(int i=0;i<100;i++){
-			
-			random = rand.nextInt(100);
-			System.out.println(random);
-		}
+		 for (int i = 0; i <= 'd'; i++) { 
+            System.out.println(i);
+        }
+		
 	}
 }
