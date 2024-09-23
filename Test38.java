0cba9d9e619ef4814e81a31ed51274b3f4dd5f91
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-23 22:53:24 +0530

    Update Test38.java

--- Test38.java
+++ Test38.java
@@ -1,10 +1,11 @@
-
+import java.util.Scanner;
 class Test38{
 	public static void main(String args[]){
-		String array []= new String['d'];
-		 for (int i = array.length-array.length; i <= 'd'; i++) { 
-            System.out.println(i);
-        }
+		Scanner sc = new Scanner(System.in);
+		String studentsArray[][] = new String[10][2];
+		double subjectMarksArray[][] = new double [10][3];
+		
+		System.out.println("Enter Student (i+1) Id : ");
 		
 	}
 }
