5e98e7785cb5be80991787927b248286b33dd48f
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-26 22:28:48 +0530

    Create Test40.java

---
+++ Test40.java
@@ -0,0 +1,9 @@
+
+public class Test40 {
+    public static void main(String[] args) {
+        
+        for (int steps = 100; steps <= 1000; steps += 100) {
+            System.out.println("User has walked " + steps + " steps.");
+        }
+    }
+}
