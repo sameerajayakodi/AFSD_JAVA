14b5747fb19bfd9e2cd4f679606abb58d4a723b3
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-05 14:09:43 +0530

    Create Demo3.java

---
+++ Demo/src/Demo3.java
@@ -0,0 +1,12 @@
+//methods nathi interface ekakata marker interface ekak kiyanawa
+//eka abstract method ekak thiyana interface ekakata functional interface ekak kiyanawa
+public interface Demo3 {
+    //abstract class ekak 0-100% wenakn abstract wenna puluwan but interface ekak 100% abstract
+    //meka athule thiyenna puluwan abstract methods witharai but onimanam default keyword eka use karala concreate method ekak hadanna puluwan
+     void saveEmployee();
+     void deleteEmployee();
+
+//     public default void displayEmployee(){
+//
+//     }
+}
