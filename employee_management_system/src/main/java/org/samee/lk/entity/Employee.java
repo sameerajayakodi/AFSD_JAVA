e28e96ac6346e96e743b86986f4a7a6b59fdee47
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-12-28 20:45:43 +0530

    Create Employee.java

---
+++ employee_management_system/src/main/java/org/samee/lk/entity/Employee.java
@@ -0,0 +1,34 @@
+package org.samee.lk.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private Double salary;
+
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
+
