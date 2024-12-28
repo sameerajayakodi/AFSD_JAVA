0503f1697fa952f967aa161ba8ade4bd4eb4fffa
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-12-28 20:45:44 +0530

    Create Main.java

---
+++ employee_management_system/src/main/java/org/samee/lk/Main.java
@@ -0,0 +1,12 @@
+package org.samee.lk;
+
+import org.samee.lk.controllers.DepartmentController;
+import org.samee.lk.dto.DepartmentDTO;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        DepartmentController.createDepartment();
+    }
+}
