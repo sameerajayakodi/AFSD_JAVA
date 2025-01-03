dad54916bdc657b3936bd5e52c9919c8a794fb05
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-01-03 21:33:55 +0530

    employee model created

--- employee_management_system/src/main/java/org/samee/lk/model/EmployeeModel.java
+++ employee_management_system/src/main/java/org/samee/lk/model/EmployeeModel.java
@@ -9,6 +9,7 @@ import org.samee.lk.entity.Employee;
 import org.samee.lk.util.GetSessionFactory;
 
 import java.util.Date;
+import java.util.List;
 
 public class EmployeeModel {
     private static SessionFactory sessionFactory = GetSessionFactory.getSessionFactory();
@@ -17,7 +18,6 @@ public class EmployeeModel {
         try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
-            // Fetch the department by name
             Department department = session.createQuery("FROM Department WHERE name = :name", Department.class)
                     .setParameter("name", employeeDTO.getDepartmentName())
                     .uniqueResult();
@@ -28,54 +28,74 @@ public class EmployeeModel {
                 return;
             }
 
-            // Create Employee entity and map fields
             Employee employee = new Employee();
             employee.setName(employeeDTO.getName());
             employee.setEmail(employeeDTO.getEmail());
             employee.setSalary(employeeDTO.getSalary());
-            employee.setStartDate(new Date()); // Set the current date for the start date
             employee.setDepartment(department);
 
-            // Save the employee
             session.persist(employee);
             session.getTransaction().commit();
 
-            System.out.println("Employee saved successfully: " + employee);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    public static Employee getEmployeeById(Long id) {
+    public static List<Employee> getAllEmployees() {
         try (Session session = sessionFactory.openSession()) {
-            return session.get(Employee.class, id);
+            return session.createQuery("FROM Employee", Employee.class).list();
         }
     }
 
-    public static void updateEmployee(Employee employee) {
+    public static void updateEmployee(Long id, EmployeeDTO employeeDTO) {
         try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
-            session.merge(employee);
+
+            Employee employee = session.get(Employee.class, id);
+            if (employee == null) {
+                System.out.println("Employee not found.");
+                session.getTransaction().rollback();
+                return;
+            }
+
+            Department department = session.createQuery("FROM Department WHERE name = :name", Department.class)
+                    .setParameter("name", employeeDTO.getDepartmentName())
+                    .uniqueResult();
+
+            if (department == null) {
+                System.out.println("Department not found. Please create the department first.");
+                session.getTransaction().rollback();
+                return;
+            }
+
+            employee.setName(employeeDTO.getName());
+            employee.setEmail(employeeDTO.getEmail());
+            employee.setSalary(employeeDTO.getSalary());
+            employee.setDepartment(department);
+
+            session.update(employee);
             session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static void deleteEmployee(Long id) {
         try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
+
             Employee employee = session.get(Employee.class, id);
-            if (employee != null) {
-                session.remove(employee);
+            if (employee == null) {
+                System.out.println("Employee not found.");
+                session.getTransaction().rollback();
+                return;
             }
+
+            session.remove(employee);
             session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-
-//    public List<Employee> getAllEmployees() {
-//        try (Session session = sessionFactory.openSession()) {
-//            Query<Employee> query = session.createQuery("from Employee", Employee.class);
-//            return query.list();
-//        }
-//    }
 }
