4b1645b8377ab41d654b29bc8f8ee74c6bd64487
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-01-05 16:29:33 +0530

    student entity done

---
+++ student_management_service/src/main/java/org/samee/lk/student_management_service/entity/Student.java
@@ -0,0 +1,18 @@
+package org.samee.lk.student_management_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Student {
+   @Id
+   int id;
+   String name;
+   int age;
+}
